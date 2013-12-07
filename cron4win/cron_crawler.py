27be1d9dcfe53feb3f55af3872c7587a22f8655b
iedc <lijunli2598@gmail.com>
2013-12-07 20:12:09 +0800

    add cron jobs for windows client

---
+++ cron4win/cron_crawler.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+ 
+import sched, time
+from threading import Thread, Timer
+import subprocess
+import sys
+ 
+# IP:219.224.135.124
+START_ID = 5000000
+END_ID = 10000000
+
+s = sched.scheduler(time.time, time.sleep)
+
+class Job(Thread):
+    def run(self, since_id, hourly_jobs):
+        '''主要业务执行方法'''
+        print_time()
+        if since_id > END_ID:
+            print 'crawl out of max range id'
+        elif since_id < START_ID:
+            print 'crawl out of min range id'
+        else:
+            theproc = subprocess.Popen("python auto_scrapy.py " % (since_id, since_id + hourly_jobs), shell = True)
+            theproc.communicate()
+
+
+def each_day_time(hour,min,sec,next_day=True):
+    '''返回当天指定时分秒的时间'''
+    struct = time.localtime()
+    if next_day:
+        day = struct.tm_mday + 1
+    else:
+        day = struct.tm_mday
+    return time.mktime((struct.tm_year,struct.tm_mon,day,
+        hour,min,sec,struct.tm_wday, struct.tm_yday,
+        struct.tm_isdst))
+    
+def print_time(name="None"):
+    print name, ":","From print_time",\
+        time.time()," :", time.ctime()
+ 
+
+def do_somthing(since_id, hourly_jobs):
+    delay = 3600 - time.time() % 3600
+    s.enter(delay, 0, do_somthing, (since_id + hourly_jobs, hourly_jobs))
+    print "-------------- auto scrapy begin running from %s to %s --------------" % (since_id, since_id + hourly_jobs)
+    job = Job()
+    job.start(since_id, hourly_jobs)  
+
+ 
+def main(since_id, hourly_jobs):
+    #指定相对时间delay后执行任务
+    priority = 0
+    delay = 0
+    s.enter(delay, priority, do_somthing, (since_id, hourly_jobs))
+    s.run()
+    while(True):
+        Timer(0, do_somthing, ()).start()
+        time.sleep(3600)
+
+
+if __name__ == "__main__":
+    since_id = int(sys.argv[1])
+    hourly_jobs = 30000
+    main(since_id, hourly_jobs)
---
+++ cron4win/scrapy_script.py
@@ -0,0 +1,66 @@
+# -*- coding: UTF-8 -*-
+
+import sys
+import importlib
+from twisted.internet import reactor
+from scrapy.crawler import Crawler
+from scrapy import log, signals
+from scrapy_weibo.spiders.user_info_v1_spider import UserInfoSpiderV1
+from scrapy.utils.project import get_project_settings
+from scrapy_weibo.settings import SPIDER_MODULES, NEWSPIDER_MODULE, \
+                                  BOT_NAME, DOWNLOAD_DELAY, CONCURRENT_ITEMS, \
+                                  CONCURRENT_REQUESTS, CONCURRENT_REQUESTS_PER_DOMAIN, \
+                                  DOWNLOAD_TIMEOUT, AUTOTHROTTLE_ENABLED, \
+                                  AUTOTHROTTLE_START_DELAY, AUTOTHROTTLE_MAX_DELAY, \
+                                  AUTOTHROTTLE_CONCURRENCY_CHECK_PERIOD, AUTOTHROTTLE_DEBUG, \
+                                  RETRY_HTTP_CODES, SPIDER_MIDDLEWARES, DOWNLOADER_MIDDLEWARES, \
+                                  ITEM_PIPELINES, EXTENSIONS, REDIS_HOST, REDIS_HOST, REDIS_PORT, \
+                                  MONGOD_HOST, MONGOD_PORT, MASTER_TIMELINE_V1_MONGOD_HOST, \
+                                  MASTER_TIMELINE_V1_MONGOD_PORT, API_KEY, API_SECRET, \
+                                  PER_TOKEN_HOURS_LIMIT, PER_IP_HOURS_LIMIT, BUFFER_SIZE, RETRY_TIMES
+
+since_id = int(sys.argv[1])
+max_id = int(sys.argv[2])
+spider = UserInfoSpiderV1(from_text=True, since_id=since_id, max_id=max_id)
+settings = get_project_settings()
+scrapy_weibo_settings_dict = {
+    'SPIDER_MODULES': SPIDER_MODULES,
+    'NEWSPIDER_MODULE': NEWSPIDER_MODULE,
+    'BOT_NAME': BOT_NAME,
+    'DOWNLOAD_DELAY': DOWNLOAD_DELAY,
+    'CONCURRENT_ITEMS': CONCURRENT_ITEMS,
+    'CONCURRENT_REQUESTS': CONCURRENT_REQUESTS,
+    'CONCURRENT_REQUESTS_PER_DOMAIN': CONCURRENT_REQUESTS_PER_DOMAIN,
+    'DOWNLOAD_TIMEOUT': DOWNLOAD_TIMEOUT,
+    'AUTOTHROTTLE_ENABLED': AUTOTHROTTLE_ENABLED,
+    'AUTOTHROTTLE_START_DELAY': AUTOTHROTTLE_START_DELAY,
+    'AUTOTHROTTLE_MAX_DELAY': AUTOTHROTTLE_MAX_DELAY,
+    'AUTOTHROTTLE_CONCURRENCY_CHECK_PERIOD': AUTOTHROTTLE_CONCURRENCY_CHECK_PERIOD,
+    'AUTOTHROTTLE_DEBUG': AUTOTHROTTLE_DEBUG,
+    'RETRY_HTTP_CODES': RETRY_HTTP_CODES,
+    'SPIDER_MIDDLEWARES': SPIDER_MIDDLEWARES,
+    'DOWNLOADER_MIDDLEWARES': DOWNLOADER_MIDDLEWARES,
+    'ITEM_PIPELINES': ITEM_PIPELINES,
+    'EXTENSIONS': EXTENSIONS,
+    'REDIS_HOST': REDIS_HOST,
+    'REDIS_PORT': REDIS_PORT,
+    'MONGOD_HOST': MONGOD_HOST,
+    'MONGOD_PORT': MONGOD_PORT,
+    'MASTER_TIMELINE_V1_MONGOD_HOST': MASTER_TIMELINE_V1_MONGOD_HOST,
+    'MASTER_TIMELINE_V1_MONGOD_PORT': MASTER_TIMELINE_V1_MONGOD_PORT,
+    'API_KEY': API_KEY,
+    'API_SECRET': API_SECRET,
+    'PER_TOKEN_HOURS_LIMIT': PER_TOKEN_HOURS_LIMIT,
+    'PER_IP_HOURS_LIMIT': PER_IP_HOURS_LIMIT,
+    'BUFFER_SIZE': BUFFER_SIZE,
+    'RETRY_TIMES': RETRY_TIMES
+}
+settings.overrides.update(scrapy_weibo_settings_dict)
+print settings.get('DOWNLOAD_DELAY')
+crawler = Crawler(settings)
+crawler.signals.connect(reactor.stop, signal=signals.spider_closed)
+crawler.configure()
+crawler.crawl(spider)
+crawler.start()
+log.start()
+reactor.run() # the script will block here until the spider_closed signal was sent
