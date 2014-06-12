0f2dc9529f29380a85b7d37257ca8c8430602a2e
LinHaoBuaa <1257819385@qq.com>
2014-06-12 17:01:28 +0800

    little fix

--- scrapy_weibo/spiders/user_timeline_v1_spider.py
+++ scrapy_weibo/spiders/user_timeline_v1_spider.py
@@ -15,15 +15,33 @@ API_KEY = '3105114937'
 BASE_URL = 'http://api.t.sina.com.cn/statuses/user_timeline.json?user_id={uid}&page={page}&count=200&source=' + API_KEY
 
 class UserTimelineV1(BaseSpider):
-    """usage: scrapy crawl user_timeline -a since_id=3021438975589423 -a max_id=3645300671902176"""
+    """usage: scrapy crawl user_timeline_v1 -a from_text=1 -a since_id=1 -a max_id=100 -a mode=onepage or allpages"""
     name = 'user_timeline_v1'
+    
+    def __init__(self, **kw):
+        super(UserTimelineV1, self).__init__(**kw)
+        since_id = kw.get('since_id', None)
+        max_id = kw.get('max_id', None)
+        from_text = kw.get('from_text', False)
+        mode = kw.get('mode', 'onepage')
+        if from_text == '1':
+            from_text = True
+        if since_id:
+            since_id = int(since_id)
+        if max_id:
+            max_id = int(max_id)
+        self.since_id = since_id
+        self.max_id = max_id
+        self.from_text = from_text
+        self.mode = mode
 
     def start_requests(self):
-        uid = 3044080932
-        request = Request(BASE_URL.format(uid=uid, page=1))
-        request.meta['page'] = 1
-        request.meta['uid'] = uid
-        yield request
+        uids = self.prepare(self.from_text, self.since_id, self.max_id)
+        for uid in uids:
+            request = Request(BASE_URL.format(uid=uid, page=1))
+            request.meta['page'] = 1
+            request.meta['uid'] = uid
+            yield request
     
     def parse(self, response):
         page = response.meta['page']
@@ -38,11 +56,44 @@ class UserTimelineV1(BaseSpider):
         for status in resp:
             items = resp2item_v1(status)
             results.extend(items)
-
-        page += 1
-        request = Request(BASE_URL.format(uid=uid, page=page))
-        request.meta['page'] = page
-        request.meta['uid'] = uid
-        results.append(request)
+        
+        if self.mode == 'allpages':
+            page += 1
+            request = Request(BASE_URL.format(uid=uid, page=page))
+            request.meta['page'] = page
+            request.meta['uid'] = uid
+            results.append(request)
 
         return results
+
+    def prepare(self, fromtext=False, start_idx=0, end_idx=100):
+        if not fromtext:
+            host = settings.get('REDIS_HOST', REDIS_HOST)
+            port = settings.get('REDIS_PORT', REDIS_PORT)
+            self.r = _default_redis(host, port)
+
+            uids_set = UIDS_SET.format(spider=self.name)
+            log.msg(format='Load uids from %(uids_set)s', level=log.WARNING, uids_set=uids_set)
+            uids = self.r.smembers(uids_set)
+            if uids == []:
+                log.msg(format='Not load any uids from %(uids_set)s', level=log.WARNING, uids_set=uids_set)
+
+        else:
+            uids = []
+            fname = '20140609_boat_uidlist.txt'
+            log.msg(format='Load uids from %(uids_set)s', level=log.WARNING, uids_set=fname)
+            f = open('./source/%s' % fname)
+            count = 0
+            for line in f:
+                count += 1
+                if count >= start_idx and count <= end_idx:
+                    uids.append(int(line.strip()))
+                elif count > start_idx:
+                    break
+                else:
+                    pass
+            if uids == []:
+                log.msg(format='Not load any uids from %(uids_set)s', level=log.WARNING, uids_set=fname)
+            f.close()    
+        
+        return uids
